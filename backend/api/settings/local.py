de842ca1037e12ce9bdc6dcfc06abf408c481351
Matthew Kokotovich <mjkokotovich@spscommerce.com>
2018-10-04 12:18:04 -0500

    Changes how travis and local environment specify database

--- backend/api/settings/local.py
+++ backend/api/settings/local.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+
+import dj_database_url
+
 from api.settings.common import *
 
 SECRET_KEY = 'amazing, i have the same combination on my luggage'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
